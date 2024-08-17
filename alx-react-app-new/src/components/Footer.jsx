3e5ada399e55b1d0d6fb0faf9eaf7acd63823212
Ngugi John <ngugyjohn@gmail.com>
2024-08-17 21:28:07 +0300

    In-line styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'darkgray', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>© 2024 My Website</p>
+    </footer>
+  );
+}
+
+export default Footer;
