d8339b21cca01b089ca08c3a2f6f6b36d504f63a
Ngugi John <ngugyjohn@gmail.com>
2024-08-25 12:33:52 +0300

    Minor modifications

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
@@ -8,7 +9,9 @@ const RecipeList = () => {
       {filteredRecipes.length > 0 ? (
         filteredRecipes.map(recipe => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <h3>
+              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+            </h3>
             <p>{recipe.description}</p>
           </div>
         ))
