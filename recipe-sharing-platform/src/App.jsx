cb04b9616cafff8f2d2c8010bccf0c520e91de9e
Ngugi John <ngugyjohn@gmail.com>
2024-09-15 19:38:36 +0300

    Minor modifications

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+
+
 function App() {
   const [count, setCount] = useState(0)
 
